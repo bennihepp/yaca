2e443080df7a171fba943517f30b35ea25e70314
Benjamin Hepp <benjamin.hepp@gmail.com>
2012-06-19 15:50:38 +0200

    Added figure-scripts

---
+++ scripts/figures/population.py
@@ -0,0 +1,54 @@
+import sys
+import cPickle
+
+import numpy
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+def do_stuff(fig, axes):
+    f = open(sys.argv[1], 'r')
+    up = cPickle.Unpickler(f)
+    d = up.load()
+    f.close()
+
+    labels, total, nonControl = d['labels'], d['total'], d['nonControl']
+
+    x = 2 * numpy.arange(0, len(labels))
+
+    axes.set_xticks(x)
+    xlabels = axes.set_xticklabels(labels, rotation='90')
+
+    #plt.set_title('Population of valid cell objects')
+    axes.bar(x - 0.45, total, width=0.8, align='center', color='red', label='Valid cell objects')
+    axes.bar(x + 0.45, nonControl, width=0.8, align='center', color='green', label='Non-control-like cell objects')
+    
+    axes.legend(loc=0)
+    
+    axes.set_xlim(-1.2, 2*len(labels) - 0.8)
+    axes.set_ylabel('Number of cells', rotation='90')
+    axes.grid(True)
+
+params = {
+#    'axes.labelsize': 10,
+#    'text.fontsize': 10,
+    'legend.fontsize': 12,
+#    'xtick.labelsize': 8,
+#    'ytick.labelsize': 8,
+    'text.usetex': False,
+    'figure.figsize': [8,6]}
+
+plt.rcParams.update(params)
+
+fig = plt.figure()
+axes = fig.add_subplot(111)
+do_stuff(fig, axes)
+plt.show()
+
+outputfile = '.'.join(sys.argv[1].split('.')[:-1]) + '.pdf'
+pp = PdfPages(outputfile)
+plt.switch_backend('cairo.pdf')
+fig = plt.figure()
+axes = fig.add_subplot(111)
+do_stuff(fig, axes)
+pp.savefig(fig)
+pp.close()
---
+++ scripts/figures/slink_runtime.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+import time
+
+import hcluster
+
+def test_cluster_slink(repeat, runs, dist_m):
+
+    np.random.seed(int(time.time()))
+
+    clocks = np.empty((repeat, runs))
+    times = np.empty((repeat, runs))
+
+    for i in xrange(repeat):
+        for j in xrange(runs):
+            print 'a'
+            t1 = time.time()
+            c1 = time.clock()
+            Z = hcluster.linkage(dist_m, 'single')
+            c2 = time.clock()
+            t2 = time.time()
+            print 'b'
+            dt = t2 - t1
+            dc = c2 - c1
+            clocks[i, j] = c2 - c1
+            times[i, j] = t2 - t1
+
+    mean_clock = np.mean(clocks)
+    std_clock = np.std(clocks)
+    mean_time = np.mean(times)
+    std_time = np.std(times)
+
+    print '5000 objects, 20 features: clocks=%f +- %f, times=%f +- %f' % (mean_clock, std_clock, mean_time, std_time)
+
+    return mean_time, std_time, mean_clock, std_clock
+
+#import cPickle
+#f = open('/g/pepperkok/hepp/dist_matrix_COP.pic', 'r')
+#up = cPickle.Unpickler(f)
+#dist_m = up.load()
+#f.close()
+
+import h5py
+f = h5py.File('/g/pepperkok/hepp/dist_matrix_5000_COP.hdf5', mode='r')
+root = f['dist_matrix']
+dist_m_dataset = root['dist_m']
+dist_m = np.empty(dist_m_dataset.shape, dtype=np.float32)
+dist_m_dataset.read_direct(dist_m)
+f.close()
+print 'done'
+
+repeat, runs = 3, 1
+#for k in [5,10,20,50]:
+
+print 'starting'
+
+test_cluster_slink(repeat, runs, dist_m)
